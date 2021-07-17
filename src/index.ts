766343d9dbe6e849ff70ef966fe2acb6f4875bfa
Asaf Shakarzy <asaf@crossid.io>
2021-07-18 01:07:21 +0300

    feat: 'withRequired' high order component to protect routes.
    apps can wrap components with this high order component to render only
    if user is authenticated. if user is not authenticated, this utility will
    take care of redirecting user to the login page.
    
    this component is agnostic to specific router and should work with any wrapped component.
    
    closes #4

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import CrossidAuthProvider, {
   useCrossidAuthActions,
 } from './provider'
 
+import { withAuth } from './with-auth'
+
 export {
   CrossidAuthProvider,
   AuthState,
   useCrossidAuth as useAuth,
   useCrossidAuthActions as useAuthActions,
+  withAuth,
 }
---
+++ src/with-auth.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from 'react'
+import {
+  useCrossidAuth as useAuth,
+  useCrossidAuthActions as useAuthActions,
+} from './provider'
+
+export interface AuthRequiredOpts {
+  returnTo?: string
+}
+
+export function withAuth<T>(
+  WrappedComponent: React.ComponentType<T>,
+  opts: AuthRequiredOpts = {}
+) {
+  const Comp = (props: T): JSX.Element => {
+    let rendered = useRef(false)
+    const [authenticated, setAuthenticated] = useState(false)
+    const { loading, client } = useAuth()
+    const { loginWithRedirect } = useAuthActions()
+    const { returnTo = defaultReturnTo() } = opts
+
+    useEffect(() => {
+      rendered.current = true
+      if (loading || authenticated) {
+        return
+      }
+
+      ;(async () => {
+        const at = await client?.getUser()
+        if (at && rendered) {
+          setAuthenticated(true)
+          return
+        }
+        await loginWithRedirect({}, returnTo)
+      })()
+
+      return () => {
+        rendered.current = false
+      }
+    }, [authenticated, client, loading, loginWithRedirect, returnTo])
+
+    return authenticated ? (
+      <WrappedComponent {...props} />
+    ) : (
+      <div>Loading...</div>
+    )
+  }
+
+  return Comp
+}
+
+const defaultReturnTo = (): string =>
+  `${window.location.pathname}${window.location.search}`
